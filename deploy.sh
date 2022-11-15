49a9030d2b372733f56fca49915ccfe785c65dff
unknown <wilkes861219@gmt.tw>
2022-11-15 17:24:27 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ set -e
 npm run build
 # 移動至到打包後的dist目錄 
 cd dist
-git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+git init #因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-git push -f https://github.com/chou0728/eric-project.git master:gh-pages
-//將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
+git push -f https://github.com/willes861219/weiyen_website.git main:gh-pages
+#將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
 cd 
